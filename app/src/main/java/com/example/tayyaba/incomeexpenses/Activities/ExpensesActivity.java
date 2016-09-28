c978314f51d8b63331cfb6ab440245f5afa360a3
tayyabataimur <tayyabataimur96@gmail.com>
2016-09-28 20:32:19 +0500

    dialog box added in expenses

--- app/src/main/java/com/example/tayyaba/incomeexpenses/Activities/ExpensesActivity.java
+++ app/src/main/java/com/example/tayyaba/incomeexpenses/Activities/ExpensesActivity.java
@@ -1,26 +1,44 @@
 package com.example.tayyaba.incomeexpenses.Activities;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tayyaba.incomeexpenses.ExpensesViewPager.FragmentAdapterExp;
 import com.example.tayyaba.incomeexpenses.MainActivity;
 import com.example.tayyaba.incomeexpenses.R;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
-public class ExpensesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
+public class ExpensesActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
+
+TextView pickDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
+
 
         Toolbar expToolbar=(Toolbar)findViewById(R.id.toolbar_exp);
         setSupportActionBar(expToolbar);
@@ -35,7 +53,7 @@ public class ExpensesActivity extends AppCompatActivity {
 
 
 
-       final ViewPager viewPagerExpenses = (ViewPager) findViewById(R.id.viewpagerExpenses);
+        final ViewPager viewPagerExpenses = (ViewPager) findViewById(R.id.viewpagerExpenses);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout_expenses);
         tabLayout.addTab(tabLayout.newTab().setText("By Category"));
         tabLayout.addTab(tabLayout.newTab().setText("By Day"));
@@ -76,11 +94,73 @@ public class ExpensesActivity extends AppCompatActivity {
     {
         switch (item.getItemId())
         {
-            case R.id.search:
-                //your code here
+            case R.id.add:
+                final Dialog dialog = new Dialog(this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.dialog_addnew_exp);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                MaterialSpinner spinnerCat=(MaterialSpinner)dialog.findViewById(R.id.spinner_cat_exp);
+                spinnerCat.setItems("Category 1", "Jelly Bean", "KitKat", "Lollipop", "Marshmallow");
+                spinnerCat.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+                    @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                        Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+                    }
+                });
+
+                MaterialSpinner spinnerAcc=(MaterialSpinner)dialog.findViewById(R.id.spinner_accnt_exp);
+                spinnerAcc.setItems("Account 1", "Jelly Bean", "KitKat", "Lollipop", "Marshmallow");
+                spinnerAcc.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+                    @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                        Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+                    }
+                });
+
+                 pickDate=(TextView
+                         ) dialog.findViewById(R.id.pickDate);
+                pickDate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Calendar now = Calendar.getInstance();
+                        DatePickerDialog dpd = DatePickerDialog.newInstance(
+                                ExpensesActivity.this,
+                                now.get(Calendar.YEAR),
+                                now.get(Calendar.MONTH),
+                                now.get(Calendar.DAY_OF_MONTH)
+                        );
+                        dpd.show(getFragmentManager(), "Datepickerdialog");
+
+                    }
+                });
+                dialog.show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        // On selecting a spinner item
+       // String item = adapterView.getItemAtPosition(i).toString();
+
+        // Showing selected spinner item
+      //  Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+
+        String string= dayOfMonth+ "-" + monthOfYear+ "-"+year;
+        pickDate.setText(string);
+
     }
 }
