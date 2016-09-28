a23a59360e3e08ab419845e1ae1f163567197c71
tayyabataimur <tayyabataimur96@gmail.com>
2016-09-29 00:49:51 +0500

    image selection in dialog

--- app/src/main/java/com/example/tayyaba/incomeexpenses/Activities/ExpensesActivity.java
+++ app/src/main/java/com/example/tayyaba/incomeexpenses/Activities/ExpensesActivity.java
@@ -1,22 +1,41 @@
 package com.example.tayyaba.incomeexpenses.Activities;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,13 +46,33 @@ import com.example.tayyaba.incomeexpenses.R;
 import com.jaredrummler.materialspinner.MaterialSpinner;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
-public class ExpensesActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
 
+public class ExpensesActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
+    public static String encodedimage;
 TextView pickDate;
+    ImageView showImage;
+    FloatingActionButton fab;
+    private static final String TAG = ExpensesActivity.class.getSimpleName();
+
+    private static final int INTENT_REQUEST_GET_IMAGES = 1;
+    private static final int INTENT_REQUEST_GET_N_IMAGES = 1;
+    HashSet<Uri> mMedia = new HashSet<Uri>();
+
+    private Context mContext;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -134,13 +173,122 @@ TextView pickDate;
 
                     }
                 });
+
+                fab=(FloatingActionButton)dialog.findViewById(R.id.choose_img_btn_exp);
+                 showImage=(ImageView)dialog.findViewById(R.id.show_img_view_exp);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ExpensesActivity.this);
+                        builder.setTitle("Add Photo!");
+                        builder.setItems(options, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int item) {
+                                if (options[item].equals("Take Photo")) {
+                                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                                    startActivityForResult(intent, 1);
+                                } else if (options[item].equals("Choose from Gallery")) {
+                                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                    startActivityForResult(intent, 2);
+
+                                } else if (options[item].equals("Cancel")) {
+                                    dialog.dismiss();
+                                }
+                            }
+                        });
+                        builder.show();
+
+
+                    }
+                });
+
+
+
+
                 dialog.show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
 
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    showImage.setImageBitmap(bitmap);
+
+                 //   encodedimage = BitMapToString(bitmap).replace("\\s", "");
+
+                    Log.v("EncodedImage", encodedimage);
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("path of image from gall", picturePath + "");
+                showImage.setImageBitmap(thumbnail);
+               // encodedimage = BitMapToString(thumbnail).replace("\\s", "");
+            }
+        }
+    }
+
+    public String BitMapToString(Bitmap bitmap) {
+
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        float factor = 200 / (float) fab.getWidth();
+        Bitmap bitmapReturn = Bitmap.createScaledBitmap(bitmap, 200, (int) (fab.getHeight() * factor), true);
+        bitmapReturn.compress(Bitmap.CompressFormat.JPEG, 35, baos);
+        byte[] b = baos.toByteArray();
+        String temp = Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         // On selecting a spinner item
@@ -163,4 +311,6 @@ TextView pickDate;
         pickDate.setText(string);
 
     }
+
+
 }
